c3d37340dd2bd7f07e9728c11b54c5634cb28628
bogdan-lab <lakatosh@phystech.edu>
2023-01-31 23:02:20 +0300

    Implement parsing default compiler include dirs
    Need it in order to define CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES.
    Which on its turn is needed for IWYU work

---
+++ cmake/Utils.cmake
@@ -0,0 +1,24 @@
+# Macro which defines CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES
+# Ideally variable has to be defined by the system automatically, but it
+# is not happening. MAcros will use external python script and initialize pathes
+# according to default compiler settings.
+# Note that macros holds dirty hack in order to ensure my local build!
+macro(add_standard_include_pathes)
+  if((NOT CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES) 
+      AND (CMAKE_CXX_COMPILER_ID STREQUAL "GNU" 
+            OR CMAKE_CXX_COMPILER_ID MATCHES ".*Clang"))
+    message(STATUS "Standard include directories will be initialized according to default compiler values.")
+    execute_process(COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/cmake/get_default_compiler_pathes ${CMAKE_CXX_COMPILER}
+                RESULT_VARIABLE COMMAND_RETURN
+                OUTPUT_VARIABLE CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES
+                ERROR_VARIABLE ERROR_MESSAGE)
+    if(NOT COMMAND_RETURN EQUAL 0)
+      message(STATUS "Cannot define default compiler include pathes. Reasone: ${ERROR_MESSAGE}")
+    else()
+      # Dirty hack in order to avoid including path which brakes my local build
+      list(LENGTH CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES STANDARD_INCLUDE_LENGTH)
+      math(EXPR LAST_INDEX "${STANDARD_INCLUDE_LENGTH}-1")
+      list(REMOVE_AT CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES ${LAST_INDEX})
+    endif()
+  endif()
+endmacro()
