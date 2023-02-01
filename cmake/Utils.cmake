199396b393512cda33f7716d6661901afb4362e5
bogdan-lab <lakatosh@phystech.edu>
2023-02-01 18:19:43 +0300

    WIP trying to implement IWYU as separate target

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -8,7 +8,7 @@ macro(add_standard_include_pathes)
       AND (CMAKE_CXX_COMPILER_ID STREQUAL "GNU" 
             OR CMAKE_CXX_COMPILER_ID MATCHES ".*Clang"))
     message(STATUS "Standard include directories will be initialized according to default compiler values.")
-    execute_process(COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/cmake/get_default_compiler_pathes ${CMAKE_CXX_COMPILER}
+    execute_process(COMMAND ${CMAKE_SOURCE_DIR}/cmake/get_default_compiler_pathes ${CMAKE_CXX_COMPILER}
                 RESULT_VARIABLE COMMAND_RETURN
                 OUTPUT_VARIABLE CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES
                 ERROR_VARIABLE ERROR_MESSAGE)
@@ -20,5 +20,17 @@ macro(add_standard_include_pathes)
       math(EXPR LAST_INDEX "${STANDARD_INCLUDE_LENGTH}-1")
       list(REMOVE_AT CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES ${LAST_INDEX})
     endif()
+  endif()
+endmacro()
+
+
+
+macro(target_enable_include_what_you_use TARGET)
+  find_program(INCLUDE_WHAT_YOU_USE include-what-you-use)
+  if(INCLUDE_WHAT_YOU_USE)
+    set_target_properties(${TARGET} PROPERTIES CXX_INCLUDE_WHAT_YOU_USE ${INCLUDE_WHAT_YOU_USE})
+    add_standard_include_pathes()
+  else()
+    message(WARNING "include-what-you-use requested, but executable not found")
   endif()
 endmacro()
